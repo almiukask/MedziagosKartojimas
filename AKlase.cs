c594bac2fef2a8d973a9865523310593ab98419d
almantask <almantas.karosevicius@outlook.com>
2020-12-07 21:25:09 +0200

    NameMetodas realisation

--- AKlase.cs
+++ AKlase.cs
@@ -6,9 +6,13 @@ namespace MedziagosKartojimas
 {
 	public class AKlase : BaseClass
 	{
+		char[] balses = {'a','e','i','o','u','y' };
 		public AKlase(string name) : base(name)
 		{
+			vidinisName = name;
 		}
+
+		public string vidinisName { get; set; }
 
 		public override int NameKodas(int a)
 		{
@@ -19,7 +23,17 @@ namespace MedziagosKartojimas
 
 		public override string NameMetodas()
 		{
-			throw new NotImplementedException();
+			char[]array = vidinisName.ToCharArray();
+			StringBuilder naujasZodis = new StringBuilder();
+			foreach (var ch in vidinisName)
+			{
+				foreach (var balse in balses)
+				{
+					if (ch == balse) naujasZodis.Append(Encoding.ASCII.GetBytes(ch.ToString()));
+					else naujasZodis.Append(ch);
+				}
+			}
+			return naujasZodis.ToString();
 		}
 	}
 }
