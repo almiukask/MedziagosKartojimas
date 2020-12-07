78cc1d500992086414833e04a43d7cd9fba21db1
almantask <almantas.karosevicius@outlook.com>
2020-12-07 21:12:19 +0200

    Aklase

---
+++ AKlase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedziagosKartojimas
+{
+	public class AKlase : BaseClass
+	{
+		public AKlase(string name) : base(name)
+		{
+		}
+
+		public override int NameKodas(int a)
+		{
+			
+			throw new NotImplementedException();
+		}
+
+
+		public override string NameMetodas()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
