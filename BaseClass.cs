72004db9106cbc414cfe72780aa4b0fa6fa5de1c
almantask <almantas.karosevicius@outlook.com>
2020-12-07 20:48:12 +0200

    abstraktybės

---
+++ BaseClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedziagosKartojimas
+{
+	abstract public class BaseClass
+	{
+		public BaseClass(string name)
+		{
+			Name = name;
+		}
+
+		public readonly string Name;
+		public abstract string NameMetodas();
+		public abstract int NameKodas(int a);
+
+	}
+}
