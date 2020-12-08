65b4f7f3565be353395b6917eb07a94ae430c1a1
almantask <almantas.karosevicius@outlook.com>
2020-12-08 19:21:10 +0200

    intial interface creation

---
+++ IZodisKitaip.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedziagosKartojimas
+{
+	public interface IZodisKitaip
+	{
+		public string ZodisBeBalsiu();
+		public string ZodisBePriebalsiu();
+		public string ZodisSuPakeistomisBalsemis(char symbol);
+		public string ZodisSuPakeistomisPriebalsemis(int index);
+	}
+}
