034e17c0f12b28b780c61aa1c1c587721d849b80
almantask <almantas.karosevicius@outlook.com>
2020-12-08 21:06:40 +0200

    Initial customer class

---
+++ Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MedziagosKartojimas
+{
+	public class Customer
+	{
+		public Customer()
+		{
+		}
+
+		public string Name { get; set; }
+		public string Bank { get; set; }
+		public double Balance { get; set; }
+	}
+}
